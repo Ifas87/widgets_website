44402ce803f08edb15ea13b053011aae55287879
Safi <safi.sharieff@gmail.com>
2022-08-17 21:22:58 +0400

    17-08-2022 Changes
    -Chnaged base layout of the website

--- widgets/widgets/react_stuff/frontend/src/App.js
+++ widgets/widgets/react_stuff/frontend/src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Calc from './calc'
 
 function App() {
   return (
     <div className="App">
       <div className="buffer"> <h1 className='title'>Widgets</h1> </div>
       <div className="content">
-        <div className="row"></div>
-        <div className="row"></div>
+        <div className="row">
+          <div className='elementA'></div>
+          <div className='elementA'></div>
+        </div>
+        
+        <div className="row">
+          <div className='elementA'></div>
+          <div className='elementA'></div>
+        </div>
       </div>
     </div>
   );
