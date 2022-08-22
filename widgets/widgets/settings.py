667fdc43f0c6c810e93e25982757bfee31f0183b
Safi <safi.sharieff@gmail.com>
2022-08-22 22:03:45 +0400

    Changes 22-08-2022
    Working on the second widget

--- widgets/widgets/settings.py
+++ widgets/widgets/settings.py
@@ -118,7 +118,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+# Try this approach next time
 #STATIC_ROOT = 'build/static/'
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
