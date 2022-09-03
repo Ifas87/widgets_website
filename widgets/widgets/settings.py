438fb1ab6eb18a9aeddc240699117dd098e4cfc4
Safi <safi.sharieff@gmail.com>
2022-09-03 21:04:10 +0400

    03-09-2022 Changes
    -Added a function to the React component for notes to export notes.
    -Used NPM to import the Axiom module.

--- widgets/widgets/settings.py
+++ widgets/widgets/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type 
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
- 
+  
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #STATIC_ROOT = os.path.join(BASE_DIR, "widgets/react_stuff/frontend/build/static")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "widgets/static/")]
